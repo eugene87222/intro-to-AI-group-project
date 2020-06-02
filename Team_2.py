bc77d36af8530d461b45623f55939ec323296e56
Eugene Yang <eugene87222@gmail.com>
2020-06-02 22:42:45 +0800

    finish the first version of evaluation function

--- Team_2.py
+++ Team_2.py
@@ -1,7 +1,8 @@
 import os
 import random
 import STcpClient
-from datetime import datetime
+from copy import deepcopy
+from datetime import datetime, timedelta
 
 '''
     輪到此程式移動棋子
@@ -14,7 +15,10 @@ from datetime import datetime
             r, c 表示要下棋子的座標位置 (row, column) (zero-base)
 '''
 
-cnt = 0
+# cnt = 0
+
+INF = 1e10
+MAX_DEPTH = 100
 
 CORNER = -1
 EMPTY = 0
@@ -41,7 +45,7 @@ def OutOfBoard(pos, direction):
         return True, (new_r, new_c)
     if new_c<0 or new_c>=WIDTH:
         return True, (new_r, new_c)
-    if new_r%HEIGHT in [0, 7] and new_c%WIDTH in [0, 7]:
+    if new_r in [0, HEIGHT-1] and new_c in [0, WIDTH-1]:
         return True, (new_r, new_c)
     return False, (new_r, new_c)
 
@@ -91,40 +95,119 @@ def GetValidMoves(board, is_black):
     return list(moves)
 
 
-def Max(depth, lifetime, alpha, beta):
-    if depth>1000 or datetime.datetime.now()>lifetime:
-        # 超時或太深
-        # evaluate 並 return score
-        return NotImplementedError
+def CheckFlip(board, pos, direction, is_black):
+    res = OutOfBoard(pos, direction)
+    # first element indicates whether this move is out of board or not
+    # second element indicates the position after this move
+    flip = []
+    if not res[0]:
+        pos = res[1]
+    else:
+        return False, []
+    player, opponent = (BLACK, WHITE) if is_black else (WHITE, BLACK)
+    if board[pos[0]][pos[1]] == opponent:
+        while board[pos[0]][pos[1]] == opponent:
+            flip.append(pos)
+            res = OutOfBoard(pos, direction)
+            if res[0]:
+                break
+            else:
+                pos = res[1]
+        if board[pos[0]][pos[1]] == player:
+            return True, flip
+    return False, []
+
+
+def PlaceAndFlip(board, pos, is_black):
+    player, opponent = (BLACK, WHITE) if is_black else (WHITE, BLACK)
+    new_board = deepcopy(board)
+    new_board[pos[0]][pos[1]] = player
+    for d in DIRECTIONS:
+        res = CheckFlip(new_board, pos, d, is_black)
+        if res[0]:
+            for t in res[1]:
+                new_board[t[0]][t[1]] = player
+    return new_board
+
+
+def Evaluate(board, is_black):
+    player, opponent = (BLACK, WHITE) if is_black else (WHITE, BLACK)
+    moves = GetValidMoves(board, is_black)
+    player_edge = 0
+    player_piece = 0
+    opponent_edge = 0
+    opponent_piece = 0
+    opponent_moves = GetValidMoves(board, not is_black)
+    for r in range(HEIGHT):
+        for c in range(WIDTH):
+            if board[r][c]==EMPTY or board[r][c]==CORNER:
+                continue
+            elif board[r][c] == player:
+                player_piece += 1
+                if r in [0, HEIGHT-1] or c in [0, WIDTH-1]:
+                    player_edge += 1
+            elif board[r][c] == opponent:
+                opponent_piece += 1
+                if r in [0, HEIGHT-1] or c in [0, WIDTH-1]:
+                    opponent_edge += 1
+            else:
+                print(f'ERROR, unknown value at {board[r][c]}')
+    if player_piece > opponent_piece:
+        piece_score = 1
+    elif player_piece == opponent_piece:
+        piece_score = 0
+    else:
+        piece_score = -1
+    edge_score = player_edge - opponent_edge
+    return piece_score * 2 + edge_score * 1.5
+
+
+def Max(board, is_black, depth, lifetime, alpha, beta):
+    if depth>MAX_DEPTH or datetime.now()>lifetime:
+        # time exceeded or too deep -> evaluate and return score
+        return Evaluate(board, is_black)
     v = -1e10
-    # 把可以走的每一步都列出來
-    next_steps = [None]
+    # list every valid move
+    next_steps = GetValidMoves(board, is_black)
     for step in next_steps:
-        v = max(v, Min(depth+1, lifetime, alpha, beta))
+        v = max(v, Min(board, not is_black, depth+1, lifetime, alpha, beta))
         if v >= beta:
             return v
         alpha = max(alpha, v)
     return v
 
 
-def Min(depth, lifetime, alpha, beta):
-    if depth>1000 or datetime.datetime.now()>lifetime:
-        # 超時或太深
-        # evaluate 並 return score
-        return NotImplementedError
+def Min(board, is_black, depth, lifetime, alpha, beta):
+    if depth>MAX_DEPTH or datetime.now()>lifetime:
+        # time exceeded or too deep -> evaluate and return score
+        return Evaluate(board, is_black)
     v = 1e10
-    # 把可以走的每一步都列出來
-    next_steps = [None]
+    # list every valid move
+    next_steps = GetValidMoves(board, is_black)
     for step in next_steps:
-        v = min(v, Max(depth+1, lifetime, alpha, beta))
+        v = min(v, Max(board, not is_black, depth+1, lifetime, alpha, beta))
         if v <= alpha:
             return v
         beta = min(beta, v)
     return v
 
 
-def AlphaBetaPruning():
-    pass
+def AlphaBetaPruning(board, is_black, lifetime):
+    depth = 0
+    best_move = None
+    max_score = -INF
+    score = -INF
+    moves = GetValidMoves(board, is_black)
+    for move in moves:
+        new_board = PlaceAndFlip(board, move, is_black)
+        score = Min(new_board, not is_black, depth, lifetime, max_score ,INF)
+        if score > max_score:
+            max_score = score
+            best_move = move
+        if datetime.now() > lifetime:
+            print('moves 跑到一半')
+            break
+    return move
 
 
 def GetStep(board, is_black):
@@ -134,26 +217,46 @@ def GetStep(board, is_black):
     y = random.randint(0, 7)
     return (x, y)
     '''
-    moves = GetValidMoves(board, is_black)
-    if is_black:
-        with open(f'./output_black/{cnt}.log', 'w') as file:
-            for row in board:
-                file.write(f'{row}\n')
-            file.write('\n')
-            for move in moves:
-                file.write(f'{move}\n')
-    else:
-        with open(f'./output_white/{cnt}.log', 'w') as file:
-            for row in board:
-                file.write(f'{row}\n')
-            file.write('\n')
-            for move in moves:
-                file.write(f'{move}\n')
-    return random.choice(moves)
+    # moves = GetValidMoves(board, is_black)
+    # if is_black:
+    #     with open(f'./output_black/{cnt}.log', 'w') as file:
+    #         for row in board:
+    #             for col in row:
+    #                 file.write(f'{col:>2} ')
+    #             file.write('\n')
+    #         file.write('\n')
+    #         for move in moves:
+    #             new_board = PlaceAndFlip(board, move, is_black)
+    #             file.write(f'{move}\n')
+    #             for row in new_board:
+    #                 for col in row:
+    #                     file.write(f'{col:>2} ')
+    #                 file.write('\n')
+    #             file.write('\n')
+    # else:
+    #     with open(f'./output_white/{cnt}.log', 'w') as file:
+    #         for row in board:
+    #             for col in row:
+    #                 file.write(f'{col:>2} ')
+    #             file.write('\n')
+    #         file.write('\n')
+    #         for move in moves:
+    #             new_board = PlaceAndFlip(board, move, is_black)
+    #             file.write(f'{move}\n')
+    #             for row in new_board:
+    #                 for col in row:
+    #                     file.write(f'{col:>2} ')
+    #                 file.write('\n')
+    #             file.write('\n')
+    # return random.choice(moves)
+
+    lifetime = datetime.now() + timedelta(seconds=4.4)
+    move = AlphaBetaPruning(board, is_black, lifetime)
+    return move
 
 
-os.mkdir('output_black')
-os.mkdir('output_white')
+# os.mkdir('output_black')
+# os.mkdir('output_white')
 
 while(True):
     (stop_program, id_package, board, is_black) = STcpClient.GetBoard()
@@ -162,4 +265,4 @@ while(True):
 
     Step = GetStep(board, is_black)
     STcpClient.SendStep(id_package, Step)
-    cnt += 1
+    # cnt += 1
